308fa6184a58bf653af4a2a36501480edab06648
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-12-30 21:16:19 +0100

    app

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+var morgan = require("morgan");
+
+const app = express();
+app.use(morgan("default"));
+
+
+app.get("/", (req, res) => {
+    res.send("<h1>Howdy</h1>")
+})
+
+app.listen(3000, () => {
+    console.log("Server is currently listening on port " + 3000)
+})
